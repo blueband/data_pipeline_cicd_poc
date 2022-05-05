a6ad3abca1d60ed9cb623b7a6fbc903906c715c3
Qamarudeen Muhammad <crescentgroup2000@gmail.com>
2022-05-05 12:46:09 +0100

    fmt apply

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
- # The configuration for the `remote` backend.
- terraform {
-     backend "remote" {
-#         # The name of your Terraform Cloud organization.
-        organization = "bluemoonltd"
-#
-#         # The name of the Terraform Cloud workspace to store Terraform state files in.
-         workspaces {
-           name = "gh-action-setup"
-         }
-       }
-     }
+# The configuration for the `remote` backend.
+terraform {
+  backend "remote" {
+    #         # The name of your Terraform Cloud organization.
+    organization = "bluemoonltd"
+    #
+    #         # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "gh-action-setup"
+    }
+  }
+}
 
 #     # An example resource that does nothing.
-     resource "null_resource" "test_example" {
-       triggers = {
-         value = "A example resource that does nothing!"
-       }
-     }
+resource "null_resource" "test_example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
+}
