e7880a7f3d775dab005c121f0f5c32a610ddf164
Qamarudeen Muhammad <crescentgroup2000@gmail.com>
2022-05-06 00:23:34 +0100

    remove dundee code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,3 @@
-
-#     # An example resource that does nothing.
-# resource "null_resource" "test_example" {
-#   triggers = {
-#     value = "A example resource that does nothing!"
-#   }
-# }
-
 module "compute" {
   name           = var.machine_name
   machine_type   = var.wkstation_machine_type
