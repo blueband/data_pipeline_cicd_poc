3205ced63c980d45fab5392c206106aa287e2ad5
Qamarudeen Muhammad <crescentgroup2000@gmail.com>
2022-05-05 23:51:03 +0100

    folder restructured

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,12 @@
 #     value = "A example resource that does nothing!"
 #   }
 # }
+
+module "compute" {
+  name         = var.machine_name
+  machine_type = var.wkstation_machine_type
+  zone         = var.zone
+  desired_status = var.desired_status
+  image = var.os_image
+
+}
